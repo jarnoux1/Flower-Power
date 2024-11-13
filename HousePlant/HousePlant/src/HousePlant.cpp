726369b9ca3a523ad844a6c646f74f076975e525
Joeseph Arnoux <jarnoux@cnm.edu>
2024-11-13 11:34:30 -0700

    progress

--- HousePlant/HousePlant/src/HousePlant.cpp
+++ HousePlant/HousePlant/src/HousePlant.cpp
@@ -14,24 +14,51 @@
 #include "IotClassroom_CNM.h"
 #include "Button.h"
 #include "Grove_Air_quality_Sensor.h"
-
+#include <credentials.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT/Adafruit_MQTT_SPARK.h" 
+#include "Adafruit_MQTT/Adafruit_MQTT.h"
 
 SYSTEM_MODE(SEMI_AUTOMATIC);
 bool status;
 float tempC;
 float tempF;
+float humid;
+int soilRead;
+int pumpTimer;
 int currentTime;
 int lastSecond;
-const int OLED_RESET=-1;
-Button encoButton(D3);
 bool onoff;
+int currentQual = -1;
+unsigned long duration;
+unsigned long startTime;
+unsigned long sampleTime_ms = 30000;
+unsigned long lowPulseOcc = 0;
+float ratio = 0;
+float concentration = 0;
+char degree = 0xF8;
+char perc = 0x25;
+const int soilSensor = A1; 
+const int pump = S1;
+Button encoButton(D3);
+const int OLED_RESET=-1;
+void WebPublish();
 
-Adafruit_SSD1306 display(OLED_RESET);
+AirQualitySensor aqSensor(A2);
+TCPClient TheClient;
+Adafruit_MQTT_SPARK mqtt(&TheClient,AIO_SERVER,AIO_SERVERPORT,AIO_USERNAME,AIO_KEY);
+Adafruit_MQTT_Publish pubAQ = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/Air Quality");  
+Adafruit_MQTT_Publish pubTemp = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/Temp"); 
+Adafruit_MQTT_Publish pubMoist = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/Soil Moisture"); 
+Adafruit_MQTT_Publish pubHumid = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/Humidity");
 Adafruit_BME280 bme;
+Adafruit_SSD1306 display(OLED_RESET);
 
 void setup() {
 Serial.begin(9600);
 waitFor(Serial.isConnected,10000);
+  pinMode(pump,OUTPUT);
+  pinMode(soilSensor,INPUT); 
 
 status = bme.begin(0x76);
     if (status == false) {
@@ -63,15 +90,32 @@ tempC = bme.readTemperature();
     }
     display.display();
 
-    if (encoButton.isClicked()) {
+soilRead = analogRead(soilSensor);
+humid = bme.readHumidity();
+
+Serial.printf("Moisture Level = %i\n",soilRead);
+  if(soilRead>2000) {
+    Serial.printf("pump on");
+    digitalWrite(pump,HIGH);
+    delay(500);
+    digitalWrite(pump,LOW);
+    }
+
+  if((millis()-pumpTimer)>500) {
+    Serial.printf("Pump Off");
+    digitalWrite(pump,LOW);
+    pumpTimer = millis();
+    }
+
+  if (encoButton.isClicked()) {
     onoff = !onoff;
     }
 
-    if (onoff == true) {
-    
+  if (onoff == true) {
+    digitalWrite(pump,HIGH);
     }
     else {
-    
+    digitalWrite(pump,LOW);
     }
   
 }
